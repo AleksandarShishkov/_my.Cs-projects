a0144139b14bfc4d26907d27eddbda85bacc5546
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-03-22 22:26:10 +0200

    Add files via upload
    ![1](https://user-images.githubusercontent.com/74405864/112053793-77057d80-8b5d-11eb-8ddd-fa20db946c12.png)
    
    ![2](https://user-images.githubusercontent.com/74405864/112053826-84bb0300-8b5d-11eb-9b0a-f30236ab02db.png)
    
    ![3](https://user-images.githubusercontent.com/74405864/112053854-8be21100-8b5d-11eb-8ecc-f36ff9197c42.png)
    
    ![4](https://user-images.githubusercontent.com/74405864/112053892-97353c80-8b5d-11eb-8bdd-458d8037b925.png)

---
+++ C#_Factorial_Forms_/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Factorial_Forms_
+{
+    public partial class Calculate_Factorial : Form
+    {
+
+        // integer to hold the integer from the text box
+        int num;
+
+
+        public Calculate_Factorial()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Input_box_TextChanged(object sender, EventArgs e)
+        {
+            num = Convert.ToInt32(Input_box.Text);
+        }
+
+
+        private void Calculate_Button_Click(object sender, EventArgs e)
+        {
+
+            int output_calc;
+
+            Output_label.Text = "\nFactorial of  !" + num + "  is:\n\n";
+            output_calc = calc_fact(num);
+            Output_label.Text += output_calc + "\n\n";
+            Calculate_Button.Enabled = false;
+            Calculate_Button.Text = "Try again";
+           
+        }
+
+        private void Output_label_Click(object sender, EventArgs e)
+        {
+            Output_label.Text = " ";
+        }
+
+
+
+
+        public int calc_fact(int num)
+        {
+            if(num <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * calc_fact(num - 1);
+            }
+        }
+
+
+
+
+
+        #region Menu_Strip options
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Calculate_Button.Text = "Calculate Factorial";
+            Calculate_Button.Enabled = true;
+            Output_label.Text = "---";
+
+        }
+     
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The program will terminate", "Exit", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+            Application.Exit();
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("\nCalculating the facotial of an integer", "Info", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+        }
+
+        #endregion
+
+    }
+
+}

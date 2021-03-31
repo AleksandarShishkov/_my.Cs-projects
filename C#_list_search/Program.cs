76a243ea6077b3a18891d9883f7d41f582fee752
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-03-31 17:50:14 +0300

    Update Program.cs

--- C#_list_search/Program.cs
+++ C#_list_search/Program.cs
@@ -8,9 +8,7 @@ namespace Integer_search
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+      
         [STAThread]
         static void Main()
         {

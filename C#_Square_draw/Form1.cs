bff6024a9952d1f6e400062c7677908578778bad
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-04-02 20:13:47 +0300

    Add files via upload

---
+++ C#_Square_draw/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace New_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // an instance of the Square class
+        Square square = new Square();
+
+
+        private void Height_input_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Width_input_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Show_button_Click(object sender, EventArgs e)
+        {
+            // integer to hold the converted heigh
+            int height_input;
+
+            // integer to hold the converted width
+            int width_input;
+
+            // validating if the input for the height is an integer
+            bool try_parse_height = Int32.TryParse(Height_input_box.Text, out height_input);
+
+            // boolean for heigh validation
+            bool is_height_correct;
+
+            // validating if the input for the width is an integer
+            bool try_parse_width = Int32.TryParse(Width_input_box.Text, out width_input);
+
+            // boolean for width validation
+            bool is_width_correct;
+
+            // validating the height
+            if (try_parse_height && square.set_height(height_input))
+            {
+                is_height_correct = true;
+                this.Height_input_box.BackColor = System.Drawing.Color.LightGreen;
+                this.Height_input_box.Enabled = false;
+                this.Show_button.Enabled = false;
+
+            }
+            else
+            {
+                is_height_correct = false;
+                this.Height_input_box.BackColor = System.Drawing.Color.Red;
+                this.Height_input_box.Enabled = false;
+                this.Show_button.Text = ". . .";
+                this.Show_button.Enabled = false;
+            }
+
+
+            // validating the width
+            if (try_parse_width && square.set_width(width_input))
+            {
+                is_width_correct = true;
+                this.Width_input_box.BackColor = System.Drawing.Color.LightGreen;
+                this.Width_input_box.Enabled = false;
+                this.Show_button.Enabled = false;
+            }
+            else
+            {
+                is_width_correct = false;
+                this.Width_input_box.BackColor = System.Drawing.Color.Red;
+                this.Width_input_box.Enabled = false;
+                this.Show_button.Text = ". . .";
+                this.Show_button.Enabled = false;
+            }
+
+
+            if(!is_height_correct)
+            {
+                MessageBox.Show($"'{height_input}' cannot be a valid height." +
+                    $"Try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                this.Height_input_box.Text = "";
+                this.Height_input_box.BackColor = System.Drawing.SystemColors.InactiveBorder;
+                this.Height_input_box.Enabled = true;
+
+                this.Width_input_box.Text = "";
+                this.Width_input_box.BackColor = System.Drawing.SystemColors.InactiveBorder;
+                this.Width_input_box.Enabled = true;
+
+                this.Show_button.Text = "Show";
+                this.Show_button.Enabled = true;
+
+
+            }else if(!is_width_correct)
+            {
+                MessageBox.Show($"'{width_input}' cannot be a valid width." +
+                    $"Try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                this.Height_input_box.Text = "";
+                this.Height_input_box.BackColor = System.Drawing.SystemColors.InactiveBorder;
+                this.Height_input_box.Enabled = true;
+
+                this.Width_input_box.Text = "";
+                this.Width_input_box.BackColor = System.Drawing.SystemColors.InactiveBorder;
+                this.Width_input_box.Enabled = true;
+
+                this.Show_button.Text = "Show";
+                this.Show_button.Enabled = true;
+            }
+
+            
+            // string to hold the outputted 
+            string output_str = "\n\n\n";
+
+
+            // if the input is correct printing the square
+            if (is_height_correct && is_width_correct)
+            {
+
+                this.Show_button.Text = "- - -";
+                string output = square.print_square(height_input, width_input, output_str);
+
+                Output_box.SelectionAlignment = HorizontalAlignment.Center;
+                Output_box.Visible = true;
+                Output_box.Text = output;
+
+            }
+
+        }
+
+        private void Output_label_Click(object sender, EventArgs e)
+        {
+          
+
+        }
+
+
+        #region Menu_strip
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("The form is re-setting", "Re_set", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+            this.Height_input_box.Text = "";
+            this.Height_input_box.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Height_input_box.Enabled = true;
+
+            this.Width_input_box.Text = "";
+            this.Width_input_box.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Width_input_box.Enabled = true;
+
+            this.Show_button.Text = "Show";
+            this.Show_button.Enabled = true;
+
+            Output_box.Text = "";
+            Output_box.Visible = false;
+
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A program that draws \nuser-defined square", "About", 
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You`ve exited the program", "Exit", 
+                MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+            Application.Exit();
+        }
+        #endregion
+
+    }
+}
---
+++ C#_Square_draw/Square.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_project
+{
+    internal class Square
+    {
+
+
+        // private ineteger for the square`s height
+        private int height;
+
+        // private integer for the square`s width
+        private int width;
+
+
+        // default construcot
+        public Square()
+        {
+            this.height = 0;
+            this.width = 0;
+        }
+
+
+
+
+        // setter for the height
+        public bool set_height(int height)
+        {
+            if(height > 0)
+            {
+                this.height = height;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        // setter for the width
+        public bool set_width(int width)
+        {
+            if(width > 0)
+            {
+                this.width = width;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        // getter for the height
+        public int get_height()
+        {
+            return this.height;
+        }
+
+
+        // getter for the width
+        public int get_width()
+        {
+            return this.width;
+        }
+
+
+        // returning a string with the drawn square
+        public string print_square(int height, int width, string output_str)
+        {
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    output_str += "# ";
+                }
+
+                output_str += '\n';
+            }
+
+            return output_str;
+        }
+
+
+    }
+}
